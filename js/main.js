12c1ddd80072ac6644a1f28a0a8a1eec5e7e6163
Mark Ryder <ryddlemethis@gmail.com>
2014-04-07 18:59:40 +0100

    1st commit

--- js/main.js
+++ js/main.js
@@ -34,3 +34,19 @@ $(document).ready(function() {
     });
 
 });
+
+var BackEndServiceCaller = (function () {
+
+    // Public methods here
+    return {
+        doCallout: function () {
+
+            $.get("http://echo.jsontest.com/key/value/one/two", function (data) {
+
+                console.log(data);
+            });
+
+        }
+    }
+
+}())
