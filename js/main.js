eda71b115016c4afa7021060f850ac1367927b0c
Mark Ryder <ryddlemethis@gmail.com>
2014-04-07 20:01:18 +0100

    added jquery file to SpecRunner.html

--- js/main.js
+++ js/main.js
@@ -40,9 +40,7 @@ var BackEndServiceCaller = (function () {
     // Public methods here
     return {
         doCallout: function () {
-
             $.get("http://echo.jsontest.com/key/value/one/two", function (data) {
-
                 console.log(data);
             });
 
---
+++ test/spec/sinonSpec.js
@@ -0,0 +1,11 @@
+describe("My callout", function () {
+    it("Should hit remote endpoint", function () {
+
+        sinon.spy($, "ajax");
+        BackEndServiceCaller.doCallout();
+        var call = $.ajax.getCall(0);
+        expect (call).not.toBeNull();
+        $.ajax.restore();
+
+    })
+})
